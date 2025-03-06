a138e1a132a825c830edf5f802bb5cb9fa8e4d7d
Luca Torelli <luca.torelli@itiscuneo.eu>
2025-03-06 23:20:08 +0100

    added predictor by latitude and longitude

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import pandas as pd
 
 
 model = joblib.load("Linear_regressor_FIAT500.pkl")
+model_lat_lon = joblib.load("Linear_regressor_FIAT500_lat-lon_included.pkl")
 encoders = joblib.load("label_encoders.pkl")
 StandardScaler_X  = joblib.load("standard_scaler.pkl")
+StandardScaler_X_lat_lon  = joblib.load("standard_scaler_lat_lon.pkl")
 LabelEncoder_model = encoders["model"]
 LabelEncoder_transmission = encoders["transmission"]
 
@@ -15,6 +17,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/location')
+def location():
+    return render_template('map.html')
 
 
 @app.route('/predict', methods=['POST'])
@@ -42,6 +48,40 @@ def predict():
         user_input_to_predict = np.insert(user_input_to_predict, 2, user_input_transmission_encoded, axis=1)
         prediction = model.predict(user_input_to_predict)
         print(prediction)
+        return jsonify({
+            'price': float(prediction[0])
+        })
+        
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+@app.route('/predict_lat_long', methods=['POST'])
+def predict_lat_long():
+    print("endpoint raggiunto")
+    try:
+
+        data = request.json
+
+        model_type = data['model']
+        engine_power = data['engine_power']
+        transmission = data['transmission']
+        age_in_days = data['age_in_days']
+        km = data['km']
+        previous_owners = data['previous_owners']
+        lat = data['lat']
+        lon = data['lon']
+        
+        user_input = np.array([model_type, engine_power, transmission, age_in_days, km, previous_owners, lat, lon])
+        user_input_model_encoded = LabelEncoder_model.transform(user_input[0].reshape(-1))
+        user_input_transmission_encoded = LabelEncoder_transmission.transform(user_input[2].reshape(-1))
+        df = pd.DataFrame([user_input], columns=["model", 	"engine_power" ,	"transmission" ,	"age_in_days" 	,"km" 	,"previous_owners", "lat", "lon"])
+
+        user_input_norm = StandardScaler_X_lat_lon.transform(df[["engine_power","age_in_days", "km", "previous_owners", "lat", "lon"]])
+        user_input_to_predict = np.insert(user_input_norm, 0, user_input_model_encoded, axis=1)
+
+        user_input_to_predict = np.insert(user_input_to_predict, 2, user_input_transmission_encoded, axis=1)
+        prediction = model_lat_lon.predict(user_input_to_predict)
         return jsonify({
             'price': float(prediction[0])
         })
